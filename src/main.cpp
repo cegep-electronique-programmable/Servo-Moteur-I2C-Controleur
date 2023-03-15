9bf6661b6ee9b916971b07dea24601ee1ad9f211
Julien Boriasse <julien@boriasse.com>
2023-03-15 12:39:57 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <mbed.h>
 
 #define ADDRESSE_I2C_PAR_DEFAUT 0x23
+#define COMMANDE_ALLUMER_MOTEUR 126
+#define COMMANDE_ETEINDRE_MOTEUR 127
+#define COMMANDE_LIRE_POSITION_MOTEUR 125
 
 I2C i2c(I2C_SDA, I2C_SCL);
 
@@ -9,82 +12,141 @@ DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
 
-int main() {
-  printf("Debut du programme\r\n");
+void sequence_led_demarrage()
+{
+    led1 = 1;
+    led2 = 0;
+    led3 = 0;
+    led4 = 0;
+    ThisThread::sleep_for(200ms);
 
-  led1 = 1;
-  led2 = 0;
-  led3 = 0;
-  led4 = 0;
-  ThisThread::sleep_for(200ms);
+    led1 = 0;
+    led2 = 1;
+    led3 = 0;
+    led4 = 0;
+    ThisThread::sleep_for(200ms);
 
-  led1 = 0;
-  led2 = 1;
-  led3 = 0;
-  led4 = 0;
-  ThisThread::sleep_for(200ms);
+    led1 = 0;
+    led2 = 0;
+    led3 = 1;
+    led4 = 0;
+    ThisThread::sleep_for(200ms);
 
-  led1 = 0;
-  led2 = 0;
-  led3 = 1;
-  led4 = 0;
-  ThisThread::sleep_for(200ms);
+    led1 = 0;
+    led2 = 0;
+    led3 = 0;
+    led4 = 1;
+    ThisThread::sleep_for(200ms);
 
-  led1 = 0;
-  led2 = 0;
-  led3 = 0;
-  led4 = 1;
-  ThisThread::sleep_for(200ms);
+    led1 = 0;
+    led2 = 0;
+    led3 = 0;
+    led4 = 0;
+}
 
-  led1 = 0;
-  led2 = 0;
-  led3 = 0;
-  led4 = 0;
+int main()
+{
 
-  const int adresse_i2c_7bits = 0x23;
-  const int adresse_i2c_8bits = adresse_i2c_7bits << 1; // Nécessairement pair
+    printf("\r\n\r\n=== Debut du programme Servo-Moteur-I2C (Controleur) ===\r\n");
 
-  char cmd[1];
-  char message[10];
+    sequence_led_demarrage();
 
-  // On utilise la valeur 125 pour faire une lecture I2C
-  int8_t sequence_moteur[] = {127, 126, -90, 125, -45, 0, 125, 45, 90, 45, 125, 0, -45, -90};
-  //int8_t sequence_moteur[] = {127, 0, 125, 0, 0, 0, 0, 0, 0, 0, 0};
-  //int8_t sequence_moteur[] = {126, 10, 20, 125, 30, 40, 125};
+    const int adresse_i2c_7bits = ADDRESSE_I2C_PAR_DEFAUT;
+    const int adresse_i2c_8bits = adresse_i2c_7bits << 1; // Nécessairement pair
 
-  while(1) {
+    char cmd[1];
+    char message[10];
 
-      // Pour chaque position du moteur
-      for (uint8_t i = 0; i < sizeof(sequence_moteur); i++) {
-          if (sequence_moteur[i] == 125) {
-              // On lit la position du moteur
-              if (i2c.read(adresse_i2c_8bits, message, sizeof(message))) { // TODO: Bug in slave when reading
-                  printf("Erreur de lecture I2C.\r\n");
-              }
-              else {
-                  printf("Etat moteur %s.\r\n", message);
-              }
+    // On utilise la valeur 125 pour faire une lecture I2C
+    int8_t sequence_moteur[] = {
+        COMMANDE_ETEINDRE_MOTEUR,
+        COMMANDE_LIRE_POSITION_MOTEUR,
+        COMMANDE_ALLUMER_MOTEUR,
+        COMMANDE_LIRE_POSITION_MOTEUR,
+        -90,
+        COMMANDE_LIRE_POSITION_MOTEUR,
+        -45,
+        0,
+        COMMANDE_LIRE_POSITION_MOTEUR,
+        45,
+        90,
+        45,
+        COMMANDE_LIRE_POSITION_MOTEUR,
+        0,
+        -45,
+        -90};
 
-              // On vide le tableau
-              for (uint8_t j = 0; j < sizeof(message); j++) {
-                  message[j] = 0;
-              }
-          } else {
-            // Écrire la position dans un tableau
-            cmd[0] = sequence_moteur[i];
-            // Envoyer le premier octet du tableau à l'adresse I2C
-            if (i2c.write(adresse_i2c_8bits, cmd, 1)) {
-              printf("Erreur d'ecriture I2C.\r\n");
+    while (1)
+    {
+        // Pour chaque position du moteur
+        for (uint8_t i = 0; i < sizeof(sequence_moteur); i++)
+        {
+            switch (sequence_moteur[i])
+            {
+            case COMMANDE_ALLUMER_MOTEUR:
+                // Demande demarrage moteur
+                cmd[0] = sequence_moteur[i];
+                // Envoyer le premier octet du tableau à l'adresse I2C
+                if (i2c.write(adresse_i2c_8bits, cmd, 1))
+                {
+                    printf("=  Erreur d'ecriture I2C !\r\n");
+                }
+                else
+                {
+                    printf("=> Demande demarrage moteur.\r\n");
+                }
+                break;
+
+            case COMMANDE_ETEINDRE_MOTEUR:
+                // Demande arret moteur
+                cmd[0] = sequence_moteur[i];
+                // Envoyer le premier octet du tableau à l'adresse I2C
+                if (i2c.write(adresse_i2c_8bits, cmd, 1))
+                {
+                    printf("=  Erreur d'ecriture I2C !\r\n");
+                }
+                else
+                {
+                    printf("=> Demande arret moteur.\r\n");
+                }
+                break;
+
+            case COMMANDE_LIRE_POSITION_MOTEUR:
+                // On lit la position du moteur (utiliser la meme taille pour le buffer du peripherique)
+                if (i2c.read(adresse_i2c_8bits, message, sizeof(message)))
+                {
+                    printf("=  Erreur de lecture I2C !\r\n");
+                }
+                else
+                {
+                    printf("=> Demande etat moteur\r\n");
+                    printf("<= %s.\r\n", message);
+                }
+
+                // On vide le tableau
+                for (uint8_t j = 0; j < sizeof(message); j++)
+                {
+                    message[j] = 0;
+                }
+                break;
+
+            default:
+                // Écrire la position dans un tableau
+                cmd[0] = sequence_moteur[i];
+                // Envoyer le premier octet du tableau à l'adresse I2C
+                if (i2c.write(adresse_i2c_8bits, cmd, 1))
+                {
+                    printf("Erreur d'ecriture I2C.\r\n");
+                }
+                else
+                {
+                    printf("=> Moteur en position %d°.\r\n", (int8_t)cmd[0]);
+                }
+                break;
             }
-            else {
-                printf("Moteur en position %d° a l'adresse 0x%02X sur 8 bits (ou 0x%02X en 7 bits).\r\n", cmd[0], adresse_i2c_8bits, adresse_i2c_7bits);
-            }
-            
-          }
-          
+
             ThisThread::sleep_for(1000ms);
-      }
-  }
-
-  
+            printf("\r\n");
+        }
+    }
 }
